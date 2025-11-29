52f3af461afc33ef84a1d5b7fb3b0c5a21ed9bd5
KhineWin <khinewin123@gmail.com>
2025-11-29 12:24:55 +0630

    get data

--- src/Welcome.vue
+++ src/Welcome.vue
@@ -1,5 +1,33 @@
 <template>
     <div>
-        Welcome
+        <ul>
+            <li v-for="p in products">{{p.item_name}}</li>
+        </ul>
     </div>
 </template>
+<script>
+    import axios from "axios"
+    export default{
+        mounted() {
+            this.fetchProducts();
+        },
+        data(){
+            return{
+                products : []
+            }
+        },
+        methods:{
+            fetchProducts(){
+                axios.get("http://localhost:8000/api/products")
+                .then((res)=>{
+                 ///   console.log(res)
+                    const data=res.data;
+                    this.products=data;
+                })
+                .catch((err)=>{
+                    console.log("error" + err)
+                });
+            }
+        }
+    }
+</script>
