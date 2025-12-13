074789fdd8bfb0f3c6eb8cfa5908936ba803d181
KhineWin <khinewin123@gmail.com>
2025-12-13 11:59:24 +0630

    update offcanvas

--- src/Sidebar.vue
+++ src/Sidebar.vue
@@ -2,23 +2,18 @@
 
 <div class="offcanvas offcanvas-start" tabindex="-1" id="sideBar" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Admin</h5>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
-    <div>
-      Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-    </div>
-    <div class="dropdown mt-3">
-      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-        Dropdown button
-      </button>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
-    </div>
+        <ul class="list-group">
+            <li class="list-group-item list-group-item-action" data-bs-dismiss="offcanvas">
+                <router-link :to="{name: 'welcome'}" class="btn d-block text-start">Home</router-link>
+            </li>
+             <li class="list-group-item list-group-item-action" data-bs-dismiss="offcanvas">
+                <router-link :to="{name: 'createProduct'}" class="btn d-block text-start">New Item</router-link>
+            </li>
+        </ul>
   </div>
 </div>
 </template>
